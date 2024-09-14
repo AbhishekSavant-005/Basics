03955925e3950e612e23a2527a033ca940d963b1
Abhishek <abhisheksavant0@gmail.com>
2024-09-14 22:45:49 +0000

    Added variabels js file

---
+++ Variables.js
@@ -0,0 +1,12 @@
+const acccId = 123
+let accMail = "abhi@gmail.com"
+var accPass = "Abhi123"
+accLoc = "Bangalore"
+
+// acccId = 2
+// console.log(acccId, accMail)
+
+accMail = "ab@ab"
+accPass = 23123
+accLoc = "BTM"
+console.table([acccId, accMail, accPass, accLoc])
