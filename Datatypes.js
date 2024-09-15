f9d298ea829f15259f757ee2c5710ebd25387bf2
Abhishek <abhisheksavant0@gmail.com>
2024-09-15 11:45:53 +0000

    This js file containes of datatypes and type conversions

---
+++ Datatypes.js
@@ -0,0 +1,11 @@
+let name = " Abhishek"
+let age = 22
+let isAdult = false
+let percentage = 98.51212313
+let status = null
+console.log(typeof undefined)
+
+let score = undefined
+let convNum = Number(score)
+console.log(typeof convNum)
+console.log(convNum)
