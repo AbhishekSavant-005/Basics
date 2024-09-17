18c13c166ac4c7272d2fd56bf923475495def95a
Abhishek <abhisheksavant0@gmail.com>
2024-09-17 17:43:34 +0000

    Learnt about date and time functions in JS

--- Number_Math.js
+++ Number_Math.js
@@ -10,10 +10,42 @@ const num = 12356.112338
 // console.log(Math.floor(4.2));   // returns current whole number -> 4.23 => 4
 // console.log(Math.min(8,3,-2,0), Math.max(1123,50,-564));  // returns min and max number within array
 
-console.log(Math.random());
-console.log((Math.random()*10) + 1);
-console.log(Math.floor(Math.random()*10) + 1);
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
 
 
 const min = 10, max = 20
-console.log(Math.floor(Math.random() * (max - min +1)) + min)
+// console.log(Math.floor(Math.random() * (max - min +1)) + min)
+
+
+// Date  and   Time
+
+  let mydate = new Date()
+//   console.log(mydate.toString())
+//   console.log(mydate.toDateString())
+//   console.log(mydate.toTimeString())
+//   console.log(mydate.toLocaleString())
+
+ let dob = new Date(2002, 8, 2)
+// console.log(dob.toDateString());
+
+// In js, Month starts from 0, jan -> 0, dec -> 11
+
+let dob1 = new Date("01-23-2023")
+// console.log(dob1);
+// console.log(dob1.toLocaleTimeString());
+
+// creating time stamp
+let dob2 = Date.now()
+// console.log(dob2);
+// console.log(dob1.getTime());
+// console.log(Math.floor(Date.now() / 1000));
+
+let newdate = new Date()
+// console.log(newdate);
+// console.log(newdate.getDay());
+
+newdate.toLocaleString('default', {
+    weekday: "long",
+})
