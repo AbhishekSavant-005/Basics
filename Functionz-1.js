e69d5babdb58a64bd91de4d7b3bc9ed50cb0c17d
Abhishek <abhisheksavant0@gmail.com>
2024-09-20 11:18:14 +0000

    Creating objects/array and accessing their properrties/values in the function.

--- Functionz-1.js
+++ Functionz-1.js
@@ -17,5 +17,47 @@ function login(username){
     }
     return `${username} just logged now.`
 }
-console.log(login());  // log on console as undefined, if no value is provided.
- 
+// console.log(login());  // log on console as undefined, if no value is provided.
+
+// printing multiple arguements withouut use of loops
+// using rest method to print multiple arguements
+
+function total_products(...p1){
+    return p1
+}
+
+// passing only p1: prints 1st arguement -> Mobile
+// using rest method, prints each arguement -> [ 'Mobile', 'Laptop', 'Wires' ]
+
+// console.log(total_products("Mobile","Laptop","Wires"));
+
+const user = {
+  username: "User1",
+  membership: "General"  
+}
+
+
+//usage of anyobject in the function to access properties of any object
+function handleObj(anyobject) {
+    console.log(`User name is ${anyobject.username1} and membership is ${anyobject.memberId}`  );   
+}
+// usual method
+// handleObj(user)
+
+// able to create object within function calling
+// handleObj({username1: "Sam",
+//     memberId: 1001
+// })
+
+const newarray = [10,20,30,40,50]
+
+function returnsecval(arr){
+    return arr[1]
+}
+
+// usual way to access values of the array
+// console.log(returnsecval(newarray));
+
+// can access array, by defining within the calling functin
+// console.log(returnsecval([10,20,30,40]));
+
